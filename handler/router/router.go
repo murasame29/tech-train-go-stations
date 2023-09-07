a09bb83590212bcc314c7e288ea3ead5362c7375
murasame29 <oogiriminister@gmail.com>
2023-09-07 10:55:21 +0900

    go09:done

--- handler/router/router.go
+++ handler/router/router.go
@@ -2,9 +2,12 @@ package router
 
 import (
 	"database/sql"
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/handler"
+	"github.com/TechBowl-japan/go-stations/service"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
@@ -12,7 +15,7 @@ func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	mux := http.NewServeMux()
 	// healthzに関するルータを定義
 	healthRouter(mux)
-
+	todoRouter(mux, todoDB)
 	return mux
 }
 
@@ -21,4 +24,37 @@ func healthRouter(mux *http.ServeMux) {
 	healthz := handler.NewHealthzHandler()
 	mux.HandleFunc("/healthz", healthz.ServeHTTP)
 
+}
+
+// todoに関するルータを定義
+func todoRouter(mux *http.ServeMux, db *sql.DB) {
+	todo := handler.NewTODOHandler(service.NewTODOService(db))
+
+	mux.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		switch r.Method {
+		case http.MethodGet:
+		case http.MethodPost:
+			err = responseJson(todo.CreateTodo(w, r))
+		case http.MethodPut:
+		case http.MethodDelete:
+		default:
+			// TODO:エラーハンドリングする
+		}
+		//エラーがあった場合ログ出力して500を返す
+		if err != nil {
+			log.Println(err)
+			responseJson(w, http.StatusInternalServerError, err)
+		}
+	})
+}
+
+// 任意のstatusをヘッドに入れたレスポンスを返す
+func responseJson(w http.ResponseWriter, status int, response interface{}) error {
+	log.Println(status)
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		return err
+	}
+	return nil
 }
